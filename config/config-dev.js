d58c5d29a95c6cdaa4cb4d17210168aa648e054d
Ann Li <annli@ca.ibm.com>
2018-01-26 11:47:24 -0500

    update configs for web domain

--- config/config-dev.js
+++ config/config-dev.js
@@ -1,3 +1,4 @@
 var config = module.exports = {};
 
 config.dde_session_uri = 'https://jdcluster.us-south.containers.mybluemix.net/daas/v1/session';
+config.web_domain = 'http://localhost:3000';
--- config/config-production.js
+++ config/config-production.js
@@ -1,3 +1,4 @@
 var config = module.exports = {};
 
 config.dde_session_uri = 'https://jdcluster.us-south.containers.mybluemix.net/daas/v1/session';
+config.web_domain = 'https://dde-angnode-app.stage1.mybluemix.net';
