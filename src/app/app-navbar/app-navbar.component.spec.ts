8cc93729e7720f6e768832ed234dee9378fd6670
Ann Li <annli@ca.ibm.com>
2018-03-01 10:45:30 -0500

    fixed 1 of 11 failures

--- src/app/app-navbar/app-navbar.component.spec.ts
+++ src/app/app-navbar/app-navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AppNavbarComponent } from './app-navbar.component';
+//import { HttpClientTestingModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppNavbarComponent', () => {
   let component: AppNavbarComponent;
@@ -8,6 +11,10 @@ describe('AppNavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      //imports: [HttpClientTestingModule],
+      imports: [HttpModule,
+      HttpClientModule,
+      AngularSvgIconModule],
       declarations: [ AppNavbarComponent ]
     })
     .compileComponents();
