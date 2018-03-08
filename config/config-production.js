507921a583e28c6987d10f698a00e962925d7d30
Ann Li <annli@ca.ibm.com>
2018-03-07 20:39:28 -0500

    update url

--- config/config-production.js
+++ config/config-production.js
@@ -1,4 +1,4 @@
 var config = module.exports = {};
 
 config.dde_session_uri = 'https://ddetest-us-south.analytics.ibm.com/daas/v1/session';
-config.web_domain = 'https://dde-angnode-app.stage1.mybluemix.net';
+config.web_domain = 'https://dde-demo-test.stage1.mybluemix.net';
