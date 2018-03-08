551d5288d005b2c1804c92af20d696eba1ebadb8
Ann Li <annli@ca.ibm.com>
2018-03-08 12:17:07 -0500

    fix unit test for dde-dashboard component

--- src/app/dde-dashboard/dde-dashboard.component.spec.ts
+++ src/app/dde-dashboard/dde-dashboard.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DdeDashboardComponent } from './dde-dashboard.component';
+import { AppNavbarComponent } from '../app-navbar/app-navbar.component';
+import { DdeDialogComponent } from '../dde-dialog/dde-dialog.component';
+import { DdeDashboardBarComponent } from '../dde-dashboard-bar/dde-dashboard-bar.component';
+
+import { DdeApiService } from '../services/dde-api.service';
+import { EncryptService } from '../services/encrypt.service';
+import { DdeActionService } from '../services/dde-action.service';
 
 describe('DdeDashboardComponent', () => {
   let component: DdeDashboardComponent;
@@ -8,7 +19,23 @@ describe('DdeDashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DdeDashboardComponent ]
+      imports: [
+        HttpModule,
+        HttpClientModule,
+        AngularSvgIconModule,
+        ModalModule.forRoot(),
+      ],
+      declarations: [
+        DdeDashboardComponent,
+        AppNavbarComponent,
+        DdeDialogComponent,
+        DdeDashboardBarComponent,
+      ],
+      providers: [
+        DdeApiService,
+        EncryptService,
+        DdeActionService,
+      ],
     })
     .compileComponents();
   }));
@@ -19,9 +46,9 @@ describe('DdeDashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  /*
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-  */
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
 });
