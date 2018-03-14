9d7649ec96f4254e17c7a772ecd1416458e587fd
Keegan Costa-Correia <Keegan@Keegans-MacBook-Pro.local>
2018-03-13 20:40:29 -0400

    added product title, category value and version to instrumentation https://github.ibm.com/GearBox/DDE-ang-node-test/issues/47

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   cognos_api_js: '',
   cognos_root_url: '',
   toaster_timer: 5000,
-  segment_key: 'SCgjbhK84jkys7zrdQnBS38hY90lys5b'
+  segment_key: 'SCgjbhK84jkys7zrdQnBS38hY90lys5b',
+  version: '1.0.0',
+  bluemixLib: 'https://console.cdn.s-bluemix.net/analytics/build/bluemix-analytics.min.js'
 };
--- src/instrumentation/analytics.ts
+++ src/instrumentation/analytics.ts
@@ -3,6 +3,7 @@ import * as segment from './segment';
 import { APIAndDashboardTraits } from '../interfaces/apiAndDashboardTraits';
 import { DashboardInteractionTraits } from '../interfaces/dashboardInteractionTraits';
 import { DocumentationTraits } from '../interfaces/documentationTraits';
+import { environment } from '../environments/environment';
 
 
 @Injectable()
@@ -10,6 +11,7 @@ export class AnalyticsService {
   private sessionId: string;
   private sessionCode: string;
   private productTitle : string = 'DDE Demo';
+  private categoryValue : string = 'Offering Interface';
 
   public events = {
     APIFramework: 'API Framework',
@@ -43,12 +45,13 @@ export class AnalyticsService {
   }
 
   loadPage(pageName: string) {
-    segment.page(pageName, {name: pageName, title: pageName, productTitle: this.productTitle, categoryValue: pageName});
+    segment.page(pageName, {name: pageName, title: pageName, productTitle: this.productTitle, categoryValue: this.categoryValue, version: environment.version});
   }
 
   trackAPIAndDashboard(name: string, action: string, result: string, message: string) {
     let traits: APIAndDashboardTraits = { action: action, sessionId: this.sessionId, sessionCode: this.sessionCode,
-                                          result: result, message: message };
+                                          result: result, message: message, productTitle: this.productTitle, version: environment.version};
+
     segment.track(name, traits);
   }
 
@@ -57,18 +60,20 @@ export class AnalyticsService {
 
     if (dataSource !== null) {
       traits = { action: action, sessionId: this.sessionId, sessionCode: this.sessionCode,
-                                            result: result, message: message, dataSource: dataSource, uiElement: uiElement };
+                                            result: result, message: message, dataSource: dataSource, uiElement: uiElement,
+                                            productTitle: this.productTitle, version: environment.version};
     }
     else {
       traits = { action: action, sessionId: this.sessionId, sessionCode: this.sessionCode,
-                                            result: result, message: message, uiElement: uiElement }
+                                            result: result, message: message, uiElement: uiElement,
+                                          productTitle: this.productTitle, version: environment.version};
     }
     segment.track(name, traits);
   }
 
   trackDocumentation(document: string, url: string) {
     let traits: DocumentationTraits = { action: 'Clicked Help Resource', sessionId: this.sessionId, sessionCode: this.sessionCode,
-                                          targetUrl: url, document: document};
+                                          targetUrl: url, document: document, productTitle: this.productTitle, version: environment.version};
     segment.track(this.events.Documentation, traits);
   }
 }
