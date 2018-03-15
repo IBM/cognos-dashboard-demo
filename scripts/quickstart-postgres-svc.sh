1fe8d2fc1aa588865dfea1bb37666f8ebc2a664f
Matt Langbehn <matthew.langbehn@gmail.com>
2018-03-15 11:24:45 -0500

    Remove unnecessary scripts
    I accidentally added scripts that were not needed for this work.
    This commit fixes the mistake.
    
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- scripts/quickstart-postgres-svc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create -f kubernetes/local-volumes.yaml
-kubectl create -f kubernetes/redis.yaml
-kubectl create -f kubernetes/gitlab-postgres-svc.yaml
-kubectl get nodes
-kubectl get svc gitlab
