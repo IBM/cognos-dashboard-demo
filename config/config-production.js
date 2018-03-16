63d552d34cc32c242e9c8caed48ec4e3c6f03b51
Ann Li <annli@ca.ibm.com>
2018-03-16 16:38:19 -0400

    test with demo-test url

--- config/config-production.js
+++ config/config-production.js
@@ -1,4 +1,4 @@
 var config = module.exports = {};
 
 config.dde_session_uri = 'https://dde-us-south.analytics.ibm.com/daas/v1/session';
-config.web_domain = 'https://dynamic-dashboard-demo.ng.bluemix.net';
+config.web_domain = 'https://dynamic-dashboard-demo-test.ng.bluemix.net';
