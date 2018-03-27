19200631d8380e9828fa0b5b068fcd7b46847b3b
Ann Li <annli@ca.ibm.com>
2018-03-27 13:16:38 -0400

    fix stage1 configs for master branch

--- config/config-production.js
+++ config/config-production.js
@@ -2,4 +2,4 @@ var config = module.exports = {};
 
 config.dde_base_url = process.env.DDE_BASE_URL || 'https://dde-us-south.analytics.ibm.com';
 config.dde_session_uri = config.dde_base_url + '/daas/v1/session';
-config.web_domain = process.env.APP_BASE_URL ||  'https://dynamic-dashboard-demo.ng.bluemix.net';
+config.web_domain = process.env.APP_BASE_URL ||  'https://http://dde-angnode-app.stage1.mybluemix.net';
