0f9e93d713cc5d56ba3401c79d8394f0fd436ab9
Susan Qu <susanqu@ca.ibm.com>
2018-04-05 16:05:45 -0400

    #85 update bash scripts for css and es

--- tests/test-csslint.sh
+++ tests/test-csslint.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . -path ./node_modules -prune -o -name '*.css' -print0 | xargs -n1 -0 ./node_modules/.bin/csslint --ignore=order-alphabetical; then
+if find . -path ./node_modules -prune -o -name '*.css' ! -name 'font-awesome-animation.min.css' ! -name 'prism.css' -print0 | xargs -n1 -0 ./node_modules/.bin/csslint --ignore=order-alphabetical; then
     test_passed "$0"
 else
     test_failed "$0"
--- tests/test-eslint.sh
+++ tests/test-eslint.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . -path ./node_modules -prune -o -name '*.js' -print0 | xargs -n1 -0 ./node_modules/.bin/eslint --config .eslint.js; then
+if find . -path ./node_modules -prune -o -name '*.js' ! -name 'karma.conf.js' ! -name 'prism.js' ! -name 'protractor.conf.js' -print0 | xargs -n1 -0 ./node_modules/.bin/eslint --config .eslint.js; then
     test_passed "$0"
 else
     test_failed "$0"
