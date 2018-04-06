d66e4f02523086b8cbe0076075d20eae33af6249
Susan Qu <susanqu@ca.ibm.com>
2018-04-06 16:56:36 -0400

    #85 deleting global rule for html, placing rule on top of file

--- tests/test-html-lint.sh
+++ tests/test-html-lint.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . -path ./node_modules -prune -o \( -name '*.htm' -o -name '*.html' \) -print0 | xargs -n1 -0 -I % ./node_modules/.bin/html-lint % --verbose --strict --config .htmllintrc; then
+if find . -path ./node_modules -prune -o \( -name '*.htm' -o -name '*.html' \) -print0 | xargs -n1 -0 -I % ./node_modules/.bin/html-lint % --verbose --strict; then
     test_passed "$0"
 else
     test_failed "$0"
