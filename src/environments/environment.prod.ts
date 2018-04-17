2928422a55ff2a59080e33c60b686c22cd9ee8c0
Susan Qu <susanqu@ca.ibm.com>
2018-04-17 17:53:39 -0400

    Revert "Revert "merge linting and other updates into production branch""
    This reverts commit 24f15854c3f8d311dc9719f4c894f25b07e62206.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  cognos_api_js: 'https://ddetest-us-south.analytics.ibm.com/daas/CognosApi.js',
-  cognos_root_url: 'https://ddetest-us-south.analytics.ibm.com/daas/',
+  cognos_api_js: 'https://dde-us-south.analytics.ibm.com/daas/CognosApi.js',
+  cognos_root_url: 'https://dde-us-south.analytics.ibm.com/daas/',
   toaster_timer: 5000,
   segment_key: "${SEGMENT_KEY}",
   version: '1.0.0',
-  bluemix_analytics_js: 'https://console.cdn.stage1.s-bluemix.net/analytics/build/bluemix-analytics.min.js'
+  bluemix_analytics_js: 'https://console.cdn.s-bluemix.net/analytics/build/bluemix-analytics.min.js'
 };
